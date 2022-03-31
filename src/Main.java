44df27924298d2026775e7bafbb77710226797af
firizky29 <13520095@std.stei.itb.ac.id>
2022-03-31 20:06:22 +0700

    project structure

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//import GUI.Frame1;
+
+import javax.swing.border.Border;
+import java.awt.*;
+import java.io.IOException;
+
+// yg ini jelas isinya GUI
+public class Main
+{
+    public static void main(String[] args) {
+
+//        java.awt.EventQueue.invokeLater(//new Runnable() {
+////            public void run() {
+////                try {
+////                    //new Frame1();
+////                } catch (IOException e) {
+////                    e.printStackTrace();
+////                }
+////            }
+//        });
+    }
+}
